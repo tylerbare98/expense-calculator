13b216f836cb65e0d42dfcc791b61dfee227b6cd
Tyler Bare <tylerbare@Tylers-MacBook-Air.local>
2023-03-05 16:26:31 -0600

    All items validated.

--- src/components/ExpenseFormContainer/ExpenseFormOpen.jsx
+++ src/components/ExpenseFormContainer/ExpenseFormOpen.jsx
@@ -12,41 +12,45 @@ function ExpenseFormOpen(props) {
   const classes = useStyles();
 
   //hooks to control state: inputs is one object holding the entered form data
-  const [titleError, setTitleError] = useState(false);
-  const [amountError, setAmountError] = useState(false);
+  const [titleError, setTitleError] = useState();
+  const [amountError, setAmountError] = useState();
   const [dateError, setdateError] = useState(false);
+  const [date, setDate] = useState(dayjs());
   const [inputs, setInputs] = useState({
       title: "",  
       amount: ""   
   }); //empty JSON object of the inputs, initialized to make compiler happy
 
   //called when the value of input changes. ie) when user types something
-  const changeHandler = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    if(name==="title" && value !== "")
-      setTitleError(false)
-    setInputs(values => ({...values, [name]: value})) //spread operator just writes values in values object, overwriting with the new data
+  const titleChangeHandler = (event) => {
+    (event.target.value !== "") ? setTitleError(false) : setTitleError(true); 
+    setInputs(values => ({...values, ["title"]: event.target.value})) //spread operator just writes values in values object, overwriting with the new data
   }
+
+  const amountChangeHandler = (event) => {
+    (event.target.value > 0) ? setAmountError(false) : setAmountError(true);
+    setInputs(values => ({...values, ["amount"]: event.target.value}))
+  }
+
 
   //called when user clicks the submit button
   const submitHandler = (event) => {
     event.preventDefault(); //prevent lame default html form stuff
     console.log(inputs)
+    console.log(date)
 
     //cancel this function if all fields are not filled out
+    let returnFlag = false
     if(!inputs.title) {
       setTitleError(true)
-      return
+      returnFlag = true;
     } 
-    if(!inputs.amount){
+    if(!inputs.amount ){
       setAmountError(true) 
-      return
+      returnFlag = true;
     }
-    // if(!inputs.date) 
-    //   setDateError(true) 
-  
-  
+    if(returnFlag === true)
+      return;
 
     //clear inputs on submit
     setInputs({
@@ -67,24 +71,26 @@ function ExpenseFormOpen(props) {
   //<ExpenseForm /> component For user to add expenses
   return (
     <>
+    
     <Container maxWidth="md" >
       <Paper className={classes.paper + ' ' + classes.closedContainer}>
       
       <form noValidate onSubmit={submitHandler}>
         <Grid container spacing={3}>
           <Grid item xs={6}>
-            <TextField label="Name of Expense" fullWidth name="title" onChange={changeHandler} error={titleError}/>
+            <TextField label="Expense Name" fullWidth name="title" onChange={titleChangeHandler} error={titleError}/>
           </Grid>
           <Grid item xs={6}>
-            <TextField label="Amount" type="number" name="amount" onChange={changeHandler}
-              step="0.01" fullWidth inputProps={{ min: 0, max: 999999.99, step: 0.01 }}
+            <TextField label="Expense Amount" type="number" name="amount" onChange={amountChangeHandler} error={amountError}
+              fullWidth inputProps={{ min: 0, max: 999999.99, }}
              />
           </Grid>
           <Grid item xs={6}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <DemoContainer components={['DatePicker']}>   
                 <DatePicker
-                  onChange={changeHandler}
+                  onChange={(date) => setDate(dayjs(date))}
+                  value={date}
                   label="Date of Expense"
                 />
               </DemoContainer>
