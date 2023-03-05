f170719508b40f631e349666e693783ee1965171
Tyler Bare <tylerbare@Tylers-MacBook-Air.local>
2023-03-05 14:25:32 -0600

    first basic form validation for title

--- src/components/ExpenseFormContainer/ExpenseFormOpen.jsx
+++ src/components/ExpenseFormContainer/ExpenseFormOpen.jsx
@@ -1,47 +1,57 @@
 import "./ExpenseFormOpen.css"
 import {useState} from 'react'
 import { Container, Paper, Button, Grid, TextField } from '@material-ui/core';
-import ExpenseButton from "./ExpenseButton";
-import ErrorModal from "../../UI/ErrorModal"
 import useStyles from '../MuiStyles'
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs, { Dayjs } from 'dayjs';
 
 function ExpenseFormOpen(props) {
   const classes = useStyles();
 
   //hooks to control state: inputs is one object holding the entered form data
-  const [error, setError] = useState();
+  const [titleError, setTitleError] = useState(false);
+  const [amountError, setAmountError] = useState(false);
+  const [dateError, setdateError] = useState(false);
   const [inputs, setInputs] = useState({
       title: "",  
-      amount: "", 
-      date: ""    
+      amount: ""   
   }); //empty JSON object of the inputs, initialized to make compiler happy
 
   //called when the value of input changes. ie) when user types something
   const changeHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value;
+    if(name==="title" && value !== "")
+      setTitleError(false)
     setInputs(values => ({...values, [name]: value})) //spread operator just writes values in values object, overwriting with the new data
   }
 
   //called when user clicks the submit button
   const submitHandler = (event) => {
     event.preventDefault(); //prevent lame default html form stuff
-    
+    console.log(inputs)
+
     //cancel this function if all fields are not filled out
-    if(!inputs.title || !inputs.amount || !inputs.date){
-      setError("All fields must be filled out.")  
-      return;
+    if(!inputs.title) {
+      setTitleError(true)
+      return
+    } 
+    if(!inputs.amount){
+      setAmountError(true) 
+      return
     }
+    // if(!inputs.date) 
+    //   setDateError(true) 
+  
+  
 
     //clear inputs on submit
     setInputs({
       title: "",
-      amount: "",
-      date: ""
+      amount: ""
     });
     props.onFormSubmit(inputs); //handler-function pointer from parent. Now we pass the inputs up to the parent
 
@@ -54,69 +64,38 @@ function ExpenseFormOpen(props) {
     props.onButtonClick(false)
   }
 
-  const exitModalHandler = () => {
-    setError();
-  }
-
   //<ExpenseForm /> component For user to add expenses
   return (
     <>
-    {error && <ErrorModal errorText={error} exitModal={exitModalHandler}/>} {/*renders if error is truthy(not null)*/}
     <Container maxWidth="md" >
       <Paper className={classes.paper + ' ' + classes.closedContainer}>
       
-      <form onSubmit={submitHandler}>
+      <form noValidate onSubmit={submitHandler}>
         <Grid container spacing={3}>
           <Grid item xs={6}>
-            <TextField label="Title" fullWidth />
+            <TextField label="Name of Expense" fullWidth name="title" onChange={changeHandler} error={titleError}/>
           </Grid>
           <Grid item xs={6}>
-            <TextField label="Amount" type="number" step="0.01" fullWidth inputProps={{ min: 0, max: 999999.99, step: 0.01 }} />
+            <TextField label="Amount" type="number" name="amount" onChange={changeHandler}
+              step="0.01" fullWidth inputProps={{ min: 0, max: 999999.99, step: 0.01 }}
+             />
           </Grid>
           <Grid item xs={6}>
-            
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={['DatePicker']}>   
+                <DatePicker
+                  onChange={changeHandler}
+                  label="Date of Expense"
+                />
+              </DemoContainer>
+            </LocalizationProvider>
           </Grid>
           <Grid item xs={6} style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-            <Button variant="outlined" style={{ marginRight: 10 }}>Cancel</Button>
-            <Button variant="contained" color="primary" type="submit">Add</Button>
+            <Button variant="outlined" style={{ marginRight: 10 }} onClick={clickHandler}>Cancel</Button>
+            <Button variant="contained" color="primary" type="submit" onClick={submitHandler}>Add</Button>
           </Grid>
         </Grid>
       </form>
-
-
-      <form onSubmit={submitHandler} id="myForm">
-        {/*Row1: Title and Amount*/}
-        <div className="form-row">
-          <div className="form-section">
-            <label>Title</label><br/>
-            <input className="form-inputBox" type="text" name="title" 
-              value={inputs.title} //clears form fields with "two way binding". Reference setInputs in submitHandler func
-              onChange={changeHandler}/> {/*called when the value of input changes*/}
-          </div>
-          <div className="form-section">
-            <label>Amount</label><br/>
-            <input className="form-inputBox" type="number" name="amount" min="0"
-              value={inputs.amount} 
-              onChange={changeHandler}/>
-          </div>
-        </div>
-        
-        {/*Row2: Date and submit button*/}
-        <div className="form-row">
-          <div className="form-section" >
-            <label>Date</label><br/>
-            <input className="form-inputBox" type="date" name="date" 
-              value={inputs.date} 
-              onChange={changeHandler}/>
-          </div>
-          <div className="form-section">
-            <ExpenseButton className="form-button" buttonType="button" buttonText="Cancel" 
-                           onButtonClick={clickHandler}
-            />
-            <input type="submit" value="Add Expense" className="form-button"/>
-          </div>
-        </div>       
-      </form> 
     </Paper>
     </Container>
     </>
